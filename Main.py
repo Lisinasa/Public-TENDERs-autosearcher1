3481435c1c56f9dc3aa3e5329b81ced0733b7f9a
Aleksandras Lisinas <Lisinasa@github.com>
2019-05-26 17:55:41 +0300

    trying to open link in link

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-raktazodis="rekonstravimas"
+raktazodis="A1 Vilnius-Kaunas-Klaipėda ruožo nuo 10,000 iki 95,000 km rekonstravimas"
 import requests
 r=requests.get("https://cvpp.eviesiejipirkimai.lt/?Query=vilnius%E2%80%93kaunas%E2%80%93klaip%C4%97da&OrderingType=0&OrderingDirection=0&TypeContractId=&ProcedureSearchTypeId=&NoticeType=&PublicationType=&SectorSearchTypeId=&IncludeExpired=false&Cpvs=&TenderId=&DeadlineFromDate=&DeadlineToDate=&PublishedFromDate=2018-04-01&PublishedToDate=")
 from bs4 import BeautifulSoup
@@ -10,7 +10,7 @@ for result in results:
         print(result[1])
     elif len(result)==1:
         print(result[0])
-      elementas = result[1]
+        elementas = result[1]
     elif len(result)==1:
         elementas = result[0]
     pavadinimas=elementas.text
@@ -19,10 +19,17 @@ for result in results:
         print("Rastas projektas pagal raktazodi")
         print(elementas.text)
         print(elementas["href"])
-import requests
-r=requests.get("https://pirkimai.eviesiejipirkimai.lt/app/rfq/publicpurchase_docs.asp?PID=437241")
-from bs4 import BeautifulSoup 
-soup = BeautifulSoup(r.content, 'html.parser')
-results=soup.find_all("var", class_="details-view-action-box") 
-result=soup.find_all("a", href=True)
-print(result)
+        projektas=puslapis+adresas
+        print(projektas)
+projekto=requests.get(projektas)
+soup = BeautifulSoup(projekto.content, 'html.parser')
+page=soup.find_all("div", class_="details-view-action-box") 
+documents=soup.find_all("a", href=True)
+for documents in page:
+    if len(documents) == 2:
+        element = result[1]
+        elif len(documents)==1:
+        element = result[0]
+    grafa=element.text
+    graf_adresas=element["href"]
+    print(element.text)
